d0393ca8caace96244cb95643f56a655027f2e21
Matthew Zhang <zhangmatt22@gmail.com>
2025-10-28 02:10:33 -0500

    fixed routing for latest blogs and some divs

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,8 @@
 import { defineCollection, z } from 'astro:content';
+import { glob } from 'astro/loaders';
 
 const blogCollection = defineCollection({
+    loader: glob({pattern: '*.md', base: './src/content/blogs'}),
     schema: ({ image }) => z.object({
         // layout: z.string(),
         title: z.string(),
